a7ae185a7f2b71db0a996f16a649c88022180dcd
Jasper Lin <cutewei.lin@gmail.com>
2025-09-17 22:22:32 +0800

    [HRS-11] modify register.spec.ts

--- src/app/features/register/register.component.spec.ts
+++ src/app/features/register/register.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterComponent } from './register.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -9,6 +9,7 @@ describe('RegisterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RegisterComponent],
+      providers: [provideHttpClient()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
