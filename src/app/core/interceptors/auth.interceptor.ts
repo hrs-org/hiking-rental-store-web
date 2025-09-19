94de8e38196c5e39cd577622085772216cbc1577
Feri Setiawan <e1546606@u.nus.edu>
2025-09-19 11:49:26 +0800

    update interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -4,17 +4,18 @@ import { catchError, throwError } from 'rxjs';
 import { SKIP_AUTH } from '../tokens/auth.token';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  if (req.context.get(SKIP_AUTH)) {
-    return next(req); // skip auth header
-  }
-
-  const token = localStorage.getItem('authToken');
   let apiReq = req;
   if (environment.production) {
     apiReq = req.clone({
       url: `${environment.apiUrl}${req.url}`,
     });
   }
+
+  if (apiReq.context.get(SKIP_AUTH)) {
+    return next(apiReq); // skip auth header
+  }
+
+  const token = localStorage.getItem('authToken');
 
   if (token) {
     apiReq = apiReq.clone({
