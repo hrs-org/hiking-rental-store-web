6327f85f2a1ceb6cc01298a12032b471c2ce7e13
kritty04 <krit1_c@hotmail.com>
2025-10-12 01:57:44 +0800

    [HRS-69] Change UI

--- src/app/features/payment/payment.component.ts
+++ src/app/features/payment/payment.component.ts
@@ -1,8 +1,11 @@
-import { Component, AfterViewInit, inject } from '@angular/core';
-import { loadStripe } from '@stripe/stripe-js';
+import { Component, AfterViewInit, inject, OnDestroy } from '@angular/core';
+import { loadStripe, StripeEmbeddedCheckout } from '@stripe/stripe-js';
 import { PwaHeaderComponent } from '../../shared/components/pwa-header/pwa-header.component';
 import { PaymentService } from '../../core/services/payment.service';
 import { firstValueFrom } from 'rxjs';
+import { InfoBottomSheetComponent } from '../../shared/components/info-bottom-sheet/info-bottom-sheet.component';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-payment',
@@ -11,18 +14,38 @@ import { firstValueFrom } from 'rxjs';
   styleUrls: ['./payment.component.scss'],
   imports: [PwaHeaderComponent],
 })
-export class PaymentComponent implements AfterViewInit {
+export class PaymentComponent implements AfterViewInit, OnDestroy {
   title = 'Test Payment';
   private stripePromise = loadStripe(
     'pk_test_51SGPODCnKKTOv8A0cxsifksvBbtKMH0d1PcKMgd5fyIGvVmY8M4BytKqmmWhdga5VMqRKplGNVDTSjhimnA90OoI000TGDvBm3',
   );
   private paymentservice = inject(PaymentService);
+  private embeddedCheckout: StripeEmbeddedCheckout | null = null;
+  private bottomSheet = inject(MatBottomSheet);
+  location = inject(Location);
 
   async ngAfterViewInit() {
     await this.initializeCheckout();
   }
 
   private async initializeCheckout() {
+    if (this.embeddedCheckout) {
+      this.bottomSheet
+        .open(InfoBottomSheetComponent, {
+          data: {
+            title: 'ERROR',
+            description: 'Embedded checkout already initialized',
+          },
+        })
+        .afterDismissed()
+        .subscribe((confirmed: boolean) => {
+          if (confirmed) {
+            this.location.back();
+          }
+        });
+      console.log('Embedded checkout already initialized');
+      return;
+    }
     const stripe = await this.stripePromise;
     if (!stripe) {
       console.error('Stripe failed to load.');
@@ -38,9 +61,18 @@ export class PaymentComponent implements AfterViewInit {
       throw new Error('clientSecret must be a string');
     }
 
-    const checkout = await stripe.initEmbeddedCheckout({
+    this.embeddedCheckout = await stripe.initEmbeddedCheckout({
       clientSecret: secret,
     });
-    checkout.mount('#checkout');
+    this.embeddedCheckout.mount('#checkout');
+  }
+
+  ngOnDestroy() {
+    // cleanup
+    if (this.embeddedCheckout) {
+      this.embeddedCheckout.destroy();
+      this.embeddedCheckout = null;
+    }
+    console.log('');
   }
 }
