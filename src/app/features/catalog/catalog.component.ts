7f3fbeae81b530e582b25cce5205918de5b2297c
EvanQwQ-CMD <e1554482@u.nus.edu>
2025-10-26 12:32:59 +0800

    [HRS-87] Can Select Enddate Before Startdate

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -42,11 +42,13 @@ export class CatalogComponent implements OnInit {
 
   startDate = new Date();
   endDate = new Date();
+  today = new Date();
   selectedQuantities: Record<string, number> = {};
 
   showDateFilter = false;
 
   ngOnInit(): void {
+    this.today.setHours(0, 0, 0, 0);
     this.endDate.setDate(this.endDate.getDate() + 5);
 
     this.store.dispatch(loadCatalog({ startDate: this.startDate, endDate: this.endDate }));
