9054caa80e3c7372834793a32ce2912a61872edb
kritty04 <krit1_c@hotmail.com>
2025-10-28 22:29:25 +0800

    [HRS-96] Show package quntity

--- src/app/core/services/catalog.service.ts
+++ src/app/core/services/catalog.service.ts
@@ -65,6 +65,7 @@ export class CatalogService {
       name: pi.itemName ?? fullItem?.itemName ?? 'Unknown Item',
       available: pi.availableQuantity ?? fullItem?.availableQuantity ?? 0,
       dailyRate: pi.dailyRate ?? fullItem?.dailyRate ?? 0,
+      rentingQuantity: pi.rentingQuantity,
       items: fullItem?.children?.map((child) => this.mapItem(child, allItems)) ?? [],
     };
   }
